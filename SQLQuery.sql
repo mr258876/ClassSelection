c308dc05843d82645549589a035d74783229812b
mr258876 <2231327533@qq.com>
2021-10-11 10:29:49 +0800

    sql schema update

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -50,19 +50,53 @@ CREATE TABLE Teacher(
 
 -- 插入教师'31111111',密码'123456',权限'管理员'
 INSERT INTO Users VALUES ('31111111', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'teach0@def.xyz', 'ROLE_M');
-INSERT INTO Department VALUES ('31111111', '31111111', '张三');
+INSERT INTO Teacher VALUES ('31111111', '31111111', '张三');
 
 
 -- 创建课程表
 CREATE TABLE Class(
 	ClassID char(6) PRIMARY KEY,
-	UserName nvarchar(10) UNIQUE NOT NULL,
+	TeacherID char(8) NOT NULL,
 	DepartmentID char(3) NOT NULL,
-	TeacherName nvarchar(32) NOT NULL
-	CONSTRAINT fk_teacher_username FOREIGN KEY (UserName) REFERENCES Users(UserName) ON DELETE NO ACTION ON UPDATE CASCADE,
-	CONSTRAINT fk_teacher_department FOREIGN KEY (UserName) REFERENCES Department(DepartmentID) ON DELETE NO ACTION ON UPDATE CASCADE
+	YearSemester char(6) NOT NULL,
+	ClassName nvarchar(10) NOT NULL,
+	ClassDescription nvarchar(64) NOT NULL
+
 );
 
--- 插入教师'31111111',密码'123456',权限'管理员'
-INSERT INTO Users VALUES ('31111111', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'teach0@def.xyz', 'ROLE_M');
-INSERT INTO Department VALUES ('31111111', '31111111', '张三');
+-- 插入课程
+INSERT INTO Class VALUES ('MIS307', '31111111', 'MIS', '202101', '高级系统分析与设计', 'lol');
+
+
+-- 创建课程安排表
+CREATE TABLE ClassSchedule(
+	ClassID char(6) PRIMARY KEY,
+	YearSemester char(6) PRIMARY KEY,
+	ClassDay tinyint PRIMARY KEY CHECK (ClassDay < 8 AND ClassDay > 0),
+	StartTime time PRIMARY KEY,
+	EndTime time NOT NULL
+	ClassRoom nvarchar(10) NOT NULL,
+);
+
+-- 插入课程安排
+INSERT INTO ClassSchedule VALUES ('MIS307', '202101', 4, 14:00, 16:00, '二教101');
+
+
+-- 创建课程余量表
+CREATE TABLE ClassRemain(
+	ClassID char(6) PRIMARY KEY,
+	YearSemester char(6) PRIMARY KEY,
+	StudLimit smallint NOT NULL CHECK (StudLimit > 0 AND StudLimit < 32767),
+	Selected smallint NOT NULL CHECK (Selected >= 0 AND Selected < 32767)
+);
+
+-- 插入课程余量
+INSERT INTO ClassRemain VALUES ('MIS307', '202101', 30, 25);
+
+
+-- 创建选课表
+CREATE TABLE ClassSelection(
+	ClassID char(6) PRIMARY KEY,
+	YearSemester char(6) PRIMARY KEY,
+	StudentID char(8) PRIMARY KEY
+);
