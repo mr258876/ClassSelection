5ee57c1e14f90dd8c5c0dd24f21a62aadadcba8a
mr258876 <2231327533@qq.com>
2021-10-11 10:44:49 +0800

    sync202110111044001

---
+++ src/com/classSelection/action/classes/ExitClass.java
@@ -0,0 +1,28 @@
+package com.ClassSelection.action.classes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+
+public class ExitClass extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        JsonObject json = new JsonObject();
+        json.addProperty("success", false);
+        json.addProperty("message", "操作失败");
+        PrintWriter out = resp.getWriter();
+        out.println(json);
+    }
+}
